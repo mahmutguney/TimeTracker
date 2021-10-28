780dbdb5bdc83374d3c852c76ca65f9a9930b916
Mahmut Güney <mguney@architecht.com>
2021-10-28 11:07:21 +0300

    Initial Commit

---
+++ TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Mahmut Güney on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
